9df6a7467433a633f561c76361f724063df8dba1
Alan Casas <alancasasarevalo@gmail.com>
2021-01-09 10:03:57 +0100

    [FEATURE][HOME] * Location listener was added to get device location, all options were set in follow function

--- lib/bloc/my_current_location/my_current_location_bloc.dart
+++ lib/bloc/my_current_location/my_current_location_bloc.dart
@@ -1,16 +1,40 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 
 part 'my_current_location_event.dart';
+
 part 'my_current_location_state.dart';
 
-class MyCurrentLocationBloc extends Bloc<MyCurrentLocationEvent, MyCurrentLocationState> {
+class MyCurrentLocationBloc
+    extends Bloc<MyCurrentLocationEvent, MyCurrentLocationState> {
   MyCurrentLocationBloc() : super(MyCurrentLocationState());
+  StreamSubscription<Position> _positionSubscription;
 
-  @override
-  Stream<MyCurrentLocationState> mapEventToState(MyCurrentLocationEvent event) async* {
-    throw UnimplementedError();
+  void followInitialize() {
+    Geolocator.isLocationServiceEnabled();
+
+    // Esta escuchando siempre la posicion del usuario
+    _positionSubscription = Geolocator.getPositionStream(
+      // Opciones de la localicacion
+      forceAndroidLocationManager: true,
+      distanceFilter: 10,
+      desiredAccuracy: LocationAccuracy.high,
+      intervalDuration: Duration(seconds: 10),
+    ).listen((Position position) {
+      print(position);
+    });
   }
 
+  void unfollow() {
+    _positionSubscription?.cancel();
+  }
+
+  @override
+  Stream<MyCurrentLocationState> mapEventToState(
+      MyCurrentLocationEvent event) async* {
+    throw UnimplementedError();
+  }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,26 @@
+import 'package:Flutter_Maps/bloc/my_current_location/my_current_location_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    context.read<MyCurrentLocationBloc>().followInitialize();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    context.read<MyCurrentLocationBloc>().unfollow();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
